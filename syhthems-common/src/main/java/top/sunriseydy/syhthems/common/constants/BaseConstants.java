82aebd0d572cd326d960f64efdb467ff06bc19dd
SunriseYDY <sunriseydy@gmail.com>
2019-04-22 13:55:45 +0800

    系统基础常量

---
+++ syhthems-common/src/main/java/top/sunriseydy/syhthems/common/constants/BaseConstants.java
@@ -0,0 +1,65 @@
+package top.sunriseydy.syhthems.common.constants;
+
+/**
+ * 系统的基础常量
+ *
+ * @author SunriseYDY
+ * @date 2019-03-09 22:57
+ */
+public class BaseConstants {
+
+    /**
+     * YES 字符常量:"Y"
+     */
+    public static final String YES = "Y";
+
+    /**
+     * NO 常量："N"
+     */
+    public static final String NO = "N";
+
+    /**
+     * 日期格式常量："yyyy-MM-dd"
+     */
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+    /**
+     * 时间格式常量:"HH:mm:ss"
+     */
+    public static final String TIME_FORMAT = "HH:mm:ss";
+
+    /**
+     * 日期时间格式常量:"yyyy-MM-dd HH:mm:ss"
+     */
+    public static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * JSON UTF-8 返回头常量："application/json;charset=utf-8"
+     */
+    public static final String JSON_UTF8 = "application/json;charset=utf-8";
+
+    /**
+     * HTML UTF-8 返回头常量："text/html;charset=utf-8"
+     */
+    public static final String HTML_UTF8 = "text/html;charset=utf-8";
+
+    /**
+     * 系统默认的 order ：0
+     */
+    public static final int DEFAULT_ORDER = 0;
+
+    /**
+     * 新建用户默认的角色代码：ROLE_USER
+     */
+    public static final String DEFAULT_USER_ROLE_CODE = "ROLE_USER";
+
+    /**
+     * 新建用户的默认菜单权限：ROLE_USER
+     */
+    public static final String DEFAULT_USER_MENU_PERMISSION = "ROLE_USER";
+
+    /**
+     * 默认的用户密码；syhthems(加密过的)
+     */
+    public static final String DEFAULT_USER_PASSWORD = "$2a$10$c9vFTclwXCqg71Xu1anhEeixmr5m0.N.i.pFt96qHQKsmco2E2eBe";
+}
