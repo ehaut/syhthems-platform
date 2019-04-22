d00c81b325d509e4db511e15898258f5b80ec2bc
SunriseYDY <sunriseydy@gmail.com>
2019-04-22 19:44:35 +0800

    Web API 模块

---
+++ syhthems-web-api/src/main/java/top/sunriseydy/syhthems/web/Web.java
@@ -0,0 +1,15 @@
+package top.sunriseydy.syhthems.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SunriseYDY
+ * @date 2019-04-09 17:05
+ */
+@SpringBootApplication(scanBasePackages = {"top.sunriseydy.syhthems"})
+public class Web {
+    public static void main(String[] args) {
+        SpringApplication.run(Web.class, args);
+    }
+}
