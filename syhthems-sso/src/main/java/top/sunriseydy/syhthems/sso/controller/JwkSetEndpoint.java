f0d52c537e308a25803fd784b560ce3f88cc5b5b
SunriseYDY <sunriseydy@gmail.com>
2019-05-06 19:35:20 +0800

    添加JwkSet端点

---
+++ syhthems-sso/src/main/java/top/sunriseydy/syhthems/sso/controller/JwkSetEndpoint.java
@@ -0,0 +1,35 @@
+package top.sunriseydy.syhthems.sso.controller;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.KeyPair;
+import java.security.Principal;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Map;
+
+/**
+ * JWT Set Endpoint
+ *
+ * @author SunriseYDY
+ * @date 2019-04-24 15:48
+ */
+@FrameworkEndpoint
+public class JwkSetEndpoint {
+    private KeyPair keyPair;
+
+    public JwkSetEndpoint(KeyPair keyPair) {
+        this.keyPair = keyPair;
+    }
+
+    @GetMapping("/.well-known/jwks.json")
+    @ResponseBody
+    public Map<String, Object> getKey(Principal principal) {
+        RSAPublicKey publicKey = (RSAPublicKey) this.keyPair.getPublic();
+        RSAKey key = new RSAKey.Builder(publicKey).build();
+        return new JWKSet(key).toJSONObject();
+    }
+}
