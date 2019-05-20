7accdbce4b931dd6ebfb22323a8b92b37ac62728
SunriseYDY <sunriseydy@gmail.com>
2019-05-20 16:13:11 +0800

    [web-api]将根路径接口返回信息改为UserDetails

--- syhthems-web-api/src/main/java/top/sunriseydy/syhthems/web/controller/HomeController.java
+++ syhthems-web-api/src/main/java/top/sunriseydy/syhthems/web/controller/HomeController.java
@@ -2,10 +2,13 @@ package top.sunriseydy.syhthems.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.sunriseydy.syhthems.common.util.ResultUtils;
 import top.sunriseydy.syhthems.common.vo.ResultVO;
+import top.sunriseydy.syhthems.db.model.CustomUserDetails;
 import top.sunriseydy.syhthems.db.util.UserUtils;
 
 /**
@@ -13,13 +16,14 @@ import top.sunriseydy.syhthems.db.util.UserUtils;
  * @date 2019-04-15 10:11
  */
 @RestController
-public class HomeController {
+public class HomeController extends BaseController {
     @Autowired
     UserUtils userUtils;
 
     @RequestMapping(value = "/")
     @PreAuthorize("hasRole('ROLE_USER')")
-    public ResultVO home() {
-        return ResultUtils.success(userUtils.getCustomUserDetailsFromSecurityContextHolderWithUsername());
+    public ResultVO home(@AuthenticationPrincipal Jwt jwt) {
+        CustomUserDetails userDetails = userUtils.getCustomUserDetailsFromSecurityContextHolderWithUsername();
+        return ResultUtils.success(userDetails);
     }
 }
