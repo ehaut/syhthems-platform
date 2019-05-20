df0d68a3cb72c304befda5a6766cb2ed5e701f18
SunriseYDY <sunriseydy@gmail.com>
2019-05-20 16:14:01 +0800

    [web-api]废置WebResourceServiceConfig，将配置信息改为Spring的自动配置

--- syhthems-web-api/src/main/java/top/sunriseydy/syhthems/web/config/WebResourceServiceConfig.java
+++ syhthems-web-api/src/main/java/top/sunriseydy/syhthems/web/config/WebResourceServiceConfig.java
@@ -1,21 +1,6 @@
 package top.sunriseydy.syhthems.web.config;
 
-import org.apache.commons.io.IOUtils;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-import java.io.IOException;
-import java.nio.charset.Charset;
 
 /**
  * oauth2 资源服务器配置
@@ -24,38 +9,7 @@ import java.nio.charset.Charset;
  * @author SunriseYDY
  * @date 2019-04-10 19:23
  */
-@Configuration
-@EnableResourceServer
+// @Configuration
 public class WebResourceServiceConfig extends ResourceServerConfigurerAdapter {
-    @Override
-    public void configure(ResourceServerSecurityConfigurer config) {
-        config.tokenServices(tokenServices());
-    }
 
-    @Bean
-    public TokenStore jwtTokenStore() {
-        return new JwtTokenStore(jwtAccessTokenConverter());
-    }
-
-    @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        Resource resource = new ClassPathResource("public.txt");
-        String publicKey = null;
-        try {
-            publicKey = IOUtils.toString(resource.getInputStream(), Charset.defaultCharset());
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
-        converter.setVerifierKey(publicKey);
-        return converter;
-    }
-
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(jwtTokenStore());
-        return defaultTokenServices;
-    }
 }
