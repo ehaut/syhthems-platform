3423da930374263ed73c735ec9bb7efa35105d8d
SunriseYDY <sunriseydy@gmail.com>
2019-05-20 16:54:09 +0800

    [web]vue-router配置

--- syhthems-web/src/router.js
+++ syhthems-web/src/router.js
@@ -3,6 +3,24 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: []
+function route (path, name, component, children) {
+  return {
+    path,
+    name,
+    components: {
+      default: require(`./components/${component}`).default,
+      main: require('./components/SyhthemsMain').default,
+      toolbar: require('./components/SyhthemsToolbar').default
+    },
+    children
+  }
+}
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    route('/login', 'login', 'SyhthemsLogin')
+  ]
 })
+
+export default router
