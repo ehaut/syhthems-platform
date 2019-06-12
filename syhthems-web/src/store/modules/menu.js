5aa7bbd3535e74edaf682ccf58931884c9134aa1
SunriseYDY <sunriseydy@gmail.com>
2019-06-12 20:10:34 +0800

    [web]menu.js 将数组为空的判断改为lodash

--- syhthems-web/src/store/modules/menu.js
+++ syhthems-web/src/store/modules/menu.js
@@ -1,3 +1,5 @@
+const _ = require('lodash/core')
+
 // state
 const state = {
   rawMenus: []
@@ -6,7 +8,7 @@ const state = {
 // getters
 const getters = {
   // 获取指定parentId的rawMenus数组
-  childRawMenusof: (state, getters, rootState, rootGetters) => (parentId) => {
+  childRawMenusOf: (state, getters, rootState, rootGetters) => (parentId) => {
     return state.rawMenus.filter(rawMenu => rawMenu.parentId === parentId)
   },
 
@@ -19,7 +21,7 @@ const getters = {
   },
 
   rawMenus2Menus: (state, getters) => (rawMenus) => {
-    if (!rawMenus || !(rawMenus instanceof Array) || rawMenus.length === 0) {
+    if (!rawMenus || _.isEmpty(rawMenus)) {
       return []
     } else {
       return rawMenus.map(rawMenu => {
@@ -29,14 +31,14 @@ const getters = {
           path: rawMenu.path,
           menuComponent: rawMenu.menuComponent,
           icon: rawMenu.icon,
-          children: getters.rawMenus2Menus(getters.childRawMenusof(rawMenu.menuId))
+          children: getters.rawMenus2Menus(getters.childRawMenusOf(rawMenu.menuId))
         }
       }, getters)
     }
   },
 
   getRoutes: (state, getters) => {
-    // 过滤掉类型为按钮的数据和path为空的菜单
+    // 过滤掉类型为按钮的数据和path为空的父级菜单
     let filterRawMenus = state.rawMenus.filter(rawMenu => ((rawMenu.type === '0') && rawMenu.path))
     let routes = getters.rawMenus2Routes(filterRawMenus)
     routes.push({
