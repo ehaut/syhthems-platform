57ddee902e6ad0e9049723025c5136444a346d13
SunriseYDY <sunriseydy@gmail.com>
2019-06-13 00:02:44 +0800

    [web]添加全局加载组件

---
+++ syhthems-web/src/components/SyhthemsLoading.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-dialog
+    v-model="globalLoading"
+    persistent
+    width="300"
+    content-class="globalLoading"
+  >
+    <v-card
+      color="primary"
+      dark
+    >
+      <v-card-text>
+        请稍等...
+        <v-progress-linear
+          indeterminate
+          color="white"
+          class="mb-0"
+        />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  export default {
+    name: 'SyhthemsLoading',
+    computed: {
+      globalLoading () {
+        return this.$store.state.globalLoading
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
