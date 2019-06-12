6f423b19272210312f01fe00f8a0a16bc14c3e56
SunriseYDY <sunriseydy@gmail.com>
2019-06-13 00:04:35 +0800

    [web]修改APP.vue组件模板

--- syhthems-web/src/App.vue
+++ syhthems-web/src/App.vue
@@ -1,5 +1,41 @@
 <template>
-  <syhthems-main />
+  <v-app
+    id="inspire"
+    :dark="darkTheme"
+  >
+    <router-view name="main" />
+    <router-view name="toolbar" />
+    <v-content>
+      <v-container
+        fill-height
+        fluid
+      >
+        <v-fade-transition mode="out-in">
+          <router-view />
+        </v-fade-transition>
+      </v-container>
+    </v-content>
+    <v-footer
+      height="auto"
+      app
+      inset
+    >
+      <v-layout
+        justify-center
+        row
+        wrap
+      >
+        <v-flex
+          text-md-center
+          xs12
+          pa-2
+        >
+          &copy; {{ new Date().getFullYear() }} — <strong>Sunrise YDY</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+    <syhthems-loading />
+  </v-app>
 </template>
 
 <script>
