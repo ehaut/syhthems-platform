5231e4b827f5e6c6d6ef731385684a11c00de3b7
SunriseYDY <sunriseydy@gmail.com>
2019-06-12 20:13:19 +0800

    [web]更改 Vuetify 使用的图标字体资源，同时定义全局的 Vuetify UI 组件

--- syhthems-web/src/plugins/vuetify.js
+++ syhthems-web/src/plugins/vuetify.js
@@ -1,13 +1,40 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify, { VIcon, VSnackbar, VAlert, VDialog, VLayout,
+  VBtn, VCard, VCardActions, VCardText, VCardTitle, VSpacer } from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 import zhHans from 'vuetify/es5/locale/zh-Hans'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+const MY_ICONS = {
+  'apps': 'fas fa-th',
+  'add': 'fas fa-plus',
+  'refresh': 'fas fa-sync-alt',
+  'notifications': 'fas fa-bell',
+  'home': 'fas fa-home',
+  'stream': 'fas fa-stream',
+  'device': 'fas fa-microchip',
+  'down': 'fas fa-chevron-down',
+  'up': 'fas fa-chevron-up'
+}
 
 Vue.use(Vuetify, {
   lang: {
     locales: zhHans,
     current: 'zhHans'
   },
-  iconfont: 'md'
+  iconfont: 'fa',
+  icons: MY_ICONS,
+  components: {
+    VIcon,
+    VSnackbar,
+    VAlert,
+    VDialog,
+    VLayout,
+    VBtn,
+    VCard,
+    VCardTitle,
+    VCardText,
+    VCardActions,
+    VSpacer
+  }
 })
