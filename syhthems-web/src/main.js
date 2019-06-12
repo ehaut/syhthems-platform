e87b23afdb0b54777cff817e3d6ee1f0e2ceea60
SunriseYDY <sunriseydy@gmail.com>
2019-06-13 00:06:59 +0800

    [web]完善main.js文件

--- syhthems-web/src/main.js
+++ syhthems-web/src/main.js
@@ -3,8 +3,21 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import SyhthemsOAuth2 from './plugins/SyhthemsOAuth2'
+import config from './config'
+import './plugins/axios'
+import './plugins/vuetify-toast-snackbar'
+import './plugins/vuetify-dialog'
+import VeLine from 'v-charts/lib/line.common'
+
+Vue.component(VeLine.name, VeLine)
 
 Vue.config.productionTip = false
+
+Vue.use(SyhthemsOAuth2, {
+  ...config.oauth2,
+  store
+})
 
 new Vue({
   router,
