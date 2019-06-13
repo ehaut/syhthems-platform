cd8d84356a6e12bcb505444e0a937c5c37d291af
SunriseYDY <sunriseydy@gmail.com>
2019-06-13 11:14:55 +0800

    [web]更新侧边栏组件

--- syhthems-web/src/components/SyhthemsMain.vue
+++ syhthems-web/src/components/SyhthemsMain.vue
@@ -1,179 +1,144 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      :clipped="$vuetify.breakpoint.lgAndUp"
-      fixed
-      app
+  <v-navigation-drawer
+    v-model="drawer"
+    clipped
+    :mini-variant.sync="mini"
+    fixed
+    app
+  >
+    <v-switch
+      v-show="!mini"
+      v-model="darkTheme"
+      :label="(!darkTheme ? 'Light' : 'Dark') + ' Theme'"
+      class="ml-3"
+    />
+    <v-spacer />
+    <v-layout
+      v-show="loading"
+      align-center
+      justify-center
+      column
     >
-      <v-list dense>
-        <template v-for="item in items">
-          <v-layout
-            v-if="item.heading"
-            :key="item.heading"
-            row
-            align-center
-          >
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>
-            <v-flex
-              xs6
-              class="text-xs-center"
-            >
-              <a
-                href="#"
-                class="body-2 black--text"
-              >EDIT</a>
-            </v-flex>
-          </v-layout>
-          <v-list-group
-            v-else-if="item.children"
-            :key="item.text"
-            v-model="item.model"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon=""
-          >
-            <template v-slot:activator="">
-              <v-list-tile>
+      <v-flex
+        xs12
+        text-xs-center
+      >
+        <v-progress-circular
+          indeterminate
+        />
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex md12>
+        <v-card>
+          <v-list>
+            <template v-for="menu in menus">
+              <v-list-group
+                v-if="menu.children && menu.children.length !== 0"
+                :key="menu.menuCode"
+                :prepend-icon="menu.icon"
+              >
+                <template v-slot:activator="">
+                  <v-list-tile>
+                    <v-list-tile-content>
+                      <v-list-tile-title>{{ menu.menuName }}</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </template>
+                <v-list-tile
+                  v-for="child in menu.children"
+                  :key="child.menuCode"
+                  :to="getRouterLink(child)"
+                >
+                  <v-list-tile-action>
+                    <v-icon>{{ child.icon }}</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{ child.menuName }}</v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-list-group>
+              <v-list-tile
+                v-else
+                :key="menu.menuCode"
+                :to="getRouterLink(menu)"
+              >
+                <v-list-tile-action>
+                  <v-icon>{{ menu.icon }}</v-icon>
+                </v-list-tile-action>
                 <v-list-tile-content>
-                  <v-list-tile-title>
-                    {{ item.text }}
-                  </v-list-tile-title>
+                  <v-list-tile-title>{{ menu.menuName }}</v-list-tile-title>
                 </v-list-tile-content>
               </v-list-tile>
             </template>
-            <v-list-tile
-              v-for="(child, i) in item.children"
-              :key="i"
-              @click=""
-            >
-              <v-list-tile-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ child.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list-group>
-          <v-list-tile
-            v-else
-            :key="item.text"
-            @click=""
-          >
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>
-                {{ item.text }}
-              </v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      color="blue darken-3"
-      dark
-      app
-      fixed
-    >
-      <v-toolbar-title
-        style="width: 300px"
-        class="ml-0 pl-3"
-      >
-        <v-toolbar-side-icon @click.stop="drawer = !drawer" />
-        <span class="hidden-sm-and-down">Google Contacts</span>
-      </v-toolbar-title>
-      <v-text-field
-        flat
-        solo-inverted
-        hide-details
-        prepend-inner-icon="search"
-        label="Search"
-        class="hidden-sm-and-down"
-      />
-      <v-spacer />
-      <v-btn icon>
-        <v-icon>apps</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>notifications</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        large
-      >
-        <v-avatar
-          size="32px"
-          tile
-        >
-          <img
-            src="../assets/logo.png"
-            alt="syhthems logo"
-          >
-        </v-avatar>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <v-container
-        fluid
-        fill-height
-      >
-        <v-layout
-          justify-center
-          align-center
-        />
-      </v-container>
-    </v-content>
-  </v-app>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-navigation-drawer>
 </template>
 
 <script>
   export default {
-    props: {},
+    name: 'Main',
     data: () => ({
-      dialog: false,
-      drawer: null,
-      items: [
-        { icon: 'contacts', text: 'Contacts' },
-        { icon: 'history', text: 'Frequently contacted' },
-        { icon: 'content_copy', text: 'Duplicates' },
-        {
-          icon: 'keyboard_arrow_up',
-          'icon-alt': 'keyboard_arrow_down',
-          text: 'Labels',
-          model: true,
-          children: [
-            { icon: 'add', text: 'Create label' }
-          ]
+      loading: false
+    }),
+    computed: {
+      darkTheme: {
+        get () {
+          return this.$store.state.darkTheme
         },
-        {
-          icon: 'keyboard_arrow_up',
-          'icon-alt': 'keyboard_arrow_down',
-          text: 'More',
-          model: false,
-          children: [
-            { text: 'Import' },
-            { text: 'Export' },
-            { text: 'Print' },
-            { text: 'Undo changes' },
-            { text: 'Other contacts' }
-          ]
+        set (value) {
+          this.$store.commit('setDarkTheme', value)
+        }
+      },
+
+      drawer: {
+        get () {
+          return this.$store.state.drawer
         },
-        { icon: 'settings', text: 'Settings' },
-        { icon: 'chat_bubble', text: 'Send feedback' },
-        { icon: 'help', text: 'Help' },
-        { icon: 'phonelink', text: 'App downloads' },
-        { icon: 'keyboard', text: 'Go to the old version' }
-      ]
-    })
+        set (value) {
+          this.$store.commit('setDrawer', value)
+        }
+      },
+
+      mini: {
+        get () {
+          return this.$store.state.mini
+        },
+        set (value) {
+          this.$store.commit('setMini', value)
+        }
+      },
+
+      menus () {
+        return this.$store.getters['menu/getMenus']
+      }
+    },
+    created () {
+      console.log('SyhthemsMain created...')
+
+      if (this.$store.state.productVO) {
+        this.$store.commit('setDrawer', true)
+      } else {
+        this.$store.commit('setDrawer', false)
+      }
+    },
+    mounted () {
+      console.log('SyhthemsMain mounted...')
+    },
+    methods: {
+      getRouterLink (menu) {
+        return (!menu.path || (menu.children && menu.children.length !== 0)) ? undefined : { path: menu.path }
+      }
+    },
+
+    beforeRouteEnter (to, from, next) {
+      next(vm => {
+        //
+      })
+    }
   }
 </script>
 
