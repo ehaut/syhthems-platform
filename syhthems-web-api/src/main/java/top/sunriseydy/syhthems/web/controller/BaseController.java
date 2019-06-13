a4a892f94eadf31234e05324726fe2c7cb208b4a
SunriseYDY <sunriseydy@gmail.com>
2019-06-13 23:21:17 +0800

    [web-api]修改BaseController

--- syhthems-web-api/src/main/java/top/sunriseydy/syhthems/web/controller/BaseController.java
+++ syhthems-web-api/src/main/java/top/sunriseydy/syhthems/web/controller/BaseController.java
@@ -1,7 +1,10 @@
 package top.sunriseydy.syhthems.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import top.sunriseydy.syhthems.db.model.CustomUserDetails;
+import top.sunriseydy.syhthems.db.util.UserUtils;
 
 /**
  * 基础 Controller
@@ -10,10 +13,22 @@ import org.springframework.security.oauth2.jwt.Jwt;
  * @date 2019-04-28 11:29
  */
 public class BaseController {
+
+    @Autowired
+    UserUtils userUtils;
+
     protected static final String DEFAULT_PAGE = "1";
     protected static final String DEFAULT_PAGE_SIZE = "10";
 
     protected Jwt getCurrentJwt(@AuthenticationPrincipal Jwt jwt) {
         return jwt;
+    }
+
+    protected String getUsername() {
+        return UserUtils.getUsername();
+    }
+
+    protected CustomUserDetails getCustomUserDetails() {
+        return userUtils.getCustomUserDetailsFromSecurityContextHolderWithUsername();
     }
 }
