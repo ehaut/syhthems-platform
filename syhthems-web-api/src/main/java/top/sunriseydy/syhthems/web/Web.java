20b3ad0b95802e876f22b239a303ab4cb890250e
SunriseYDY <sunriseydy@gmail.com>
2020-03-01 15:03:44 +0800

    web api 模块微服务化

--- syhthems-web-api/src/main/java/top/sunriseydy/syhthems/web/Web.java
+++ syhthems-web-api/src/main/java/top/sunriseydy/syhthems/web/Web.java
@@ -2,12 +2,14 @@ package top.sunriseydy.syhthems.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author SunriseYDY
  * @date 2019-04-09 17:05
  */
 @SpringBootApplication(scanBasePackages = {"top.sunriseydy.syhthems"})
+@EnableEurekaClient
 public class Web {
     public static void main(String[] args) {
         SpringApplication.run(Web.class, args);
