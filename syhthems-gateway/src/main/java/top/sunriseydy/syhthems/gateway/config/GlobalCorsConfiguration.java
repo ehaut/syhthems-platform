55d0702f18476e8c0d1e1e73ceb99eea9329719a
SunriseYDY <sunriseydy@gmail.com>
2020-03-01 15:07:39 +0800

    [gateway]gateway服务添加全局CORS配置

---
+++ syhthems-gateway/src/main/java/top/sunriseydy/syhthems/gateway/config/GlobalCorsConfiguration.java
@@ -0,0 +1,26 @@
+package top.sunriseydy.syhthems.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author SunriseYDY
+ * @date 2020-03-01 13:11
+ */
+@Configuration
+public class GlobalCorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
