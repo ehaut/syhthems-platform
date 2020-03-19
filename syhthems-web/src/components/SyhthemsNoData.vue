f5be4c090cce5e7a4aedc9fb7d41b471dc83cad2
SunriseYDY <sunriseydy@gmail.com>
2020-03-19 13:46:36 +0800

    [web]添加无数据和无搜索结果的组件

---
+++ syhthems-web/src/components/SyhthemsNoData.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row>
+    <v-col
+      cols="12"
+      sm="6"
+      md="4"
+      offset="0"
+      offset-sm="3"
+      offset-md="4"
+    >
+      <v-alert
+        type="error"
+      >
+        <slot>没有数据</slot>
+      </v-alert>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    name: 'SyhthemsNoData',
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ syhthems-web/src/components/SyhthemsNoResults.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row>
+    <v-col
+      cols="12"
+      sm="6"
+      md="4"
+      offset="0"
+      offset-sm="3"
+      offset-md="4"
+    >
+      <v-alert
+        type="warning"
+      >
+        <slot>没有符合条件的结果</slot>
+      </v-alert>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    name: 'SyhthemsNoResults',
+  }
+</script>
+
+<style scoped>
+
+</style>
