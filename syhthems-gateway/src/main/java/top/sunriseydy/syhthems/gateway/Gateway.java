61a95a317d89ccf829a50588fea56f201d8a9ef7
SunriseYDY <sunriseydy@gmail.com>
2020-04-13 18:20:36 +0800

    [gateway]添加spring boot编译插件 添加common依赖

--- syhthems-gateway/src/main/java/top/sunriseydy/syhthems/gateway/Gateway.java
+++ syhthems-gateway/src/main/java/top/sunriseydy/syhthems/gateway/Gateway.java
@@ -1,14 +1,16 @@
 package top.sunriseydy.syhthems.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author SunriseYDY
  * @date 2019-12-30 23:31
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class}, scanBasePackages = {"top.sunriseydy.syhthems"})
 @EnableZuulProxy
 public class Gateway {
     public static void main(String[] args) {
