b0c19bc1d93000ce60754d88f0b85c6921613519
SunriseYDY <sunriseydy@gmail.com>
2020-04-23 21:14:26 +0800

    [common]添加服务相关常量 包括服务名和服务路由路径

---
+++ syhthems-common/src/main/java/top/sunriseydy/syhthems/common/constants/ServiceConstants.java
@@ -0,0 +1,42 @@
+package top.sunriseydy.syhthems.common.constants;
+
+/**
+ * 服务相关常量 包括服务名和服务路由路径
+ *
+ * @author SunriseYDY
+ * @date 2020-04-17 15:33
+ */
+public class ServiceConstants {
+    /**
+     * syhthems-gateway 服务名称
+     */
+    public static final String SYHTHEMS_GATEWAY_NAME = "syhthems-gateway";
+    /**
+     * syhthems-gateway 服务路径
+     */
+    public static final String SYHTHEMS_GATEWAY_PATH = "/";
+    /**
+     * syhthems-sso 服务名称
+     */
+    public static final String SYHTHEMS_SSO_NAME = "syhthems-sso";
+    /**
+     * syhthems-sso 服务路径
+     */
+    public static final String SYHTHEMS_SSO_PATH = "/sso";
+    /**
+     * syhthems-api 服务名称
+     */
+    public static final String SYHTHEMS_API_NAME = "syhthems-api";
+    /**
+     * syhthems-api 服务路径
+     */
+    public static final String SYHTHEMS_API_PATH = "/api";
+    /**
+     * syhthems-swagger 服务名称
+     */
+    public static final String SYHTHEMS_SWAGGER_NAME = "syhthems-swagger";
+    /**
+     * syhthems-swagger 服务路径
+     */
+    public static final String SYHTHEMS_SWAGGER_PATH = "/swagger";
+}
